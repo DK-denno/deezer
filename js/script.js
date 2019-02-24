e39ededddcd88b91ba4f636908d77e040459c2fd
DK-denno <dennisveer27@gmail.com>
2019-02-24 03:32:43 +0300

    almost done

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ $(document).ready(function () {
     });
 
     
+    $(document).ready(function() {
+        
+        $("#cf7_controls").on('click', 'span', function() {
+          $("#cf7 img").removeClass("opaque");
+          
+      
+          var newImage = $(this).index();
+      
+          $("#cf7 img").eq(newImage).addClass("opaque");
+      
+          $("#cf7_controls span").removeClass("selected");
+          $(this).addClass("selected");
+        });
+      });
